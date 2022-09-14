617d392ea13fb7c0ec56aa35f7c77d3130ca5db4
Daniil <daniil.sadovnikov@rambler.ru>
2022-09-14 23:27:09 +0300

    Update setup.py

--- temperature_monitor/setup.py
+++ temperature_monitor/setup.py
@@ -1,7 +1,16 @@
 from setuptools import setup, find_packages
+from sphinx.setup_command import BuildDoc
 
+cmdclass = {'build_sphinx': BuildDoc}
+name='stem'
+version='0.0.1'
 setup(
     name='stem',
     version='0.0.1',
-    packages=find_packages(include=['stem', 'stem.*'])
+    cmdclass=cmdclass,
+    packages=find_packages(include=['stem', 'stem.*']),
+    command_options={
+        'build_sphinx': {
+            'project': ('setup.py', name),
+            'version': ('setup.py', version)}}
 )
