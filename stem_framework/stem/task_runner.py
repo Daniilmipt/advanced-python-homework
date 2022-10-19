cbdbb25b8b62ed441710d05a0b6a58ee00767fcd
Daniil <daniil.sadovnikov@rambler.ru>
2022-10-20 00:18:28 +0300

    Create task_runner.py

---
+++ stem_framework/stem/task_runner.py
@@ -0,0 +1,42 @@
+import os
+from typing import Generic, TypeVar
+from abc import ABC, abstractmethod
+
+from .meta import Meta
+from .task_tree import TaskNode
+
+T = TypeVar("T")
+
+
+class TaskRunner(ABC, Generic[T]):
+
+    @abstractmethod
+    def run(self, meta: Meta, task_node: TaskNode[T]) -> T:
+        pass
+
+
+class SimpleRunner(TaskRunner[T]):
+    def run(self, meta: Meta, task_node: TaskNode[T]) -> T:
+        dc = {}
+        for dep in task_node.dependencies:
+            dc[dep.task.name] = dep.task.transform(meta)
+        return task_node.task.transform(meta, **dc)
+
+
+class ThreadingRunner(TaskRunner[T]):
+    MAX_WORKERS = 5
+
+    def run(self, meta: Meta, task_node: TaskNode[T]) -> T:
+        pass  # TODO(Assignment 9)
+
+
+class AsyncRunner(TaskRunner[T]):
+    def run(self, meta: Meta, task_node: TaskNode[T]) -> T:
+        pass  # TODO(Assignment 9)
+
+
+class ProcessingRunner(TaskRunner[T]):
+    MAX_WORKERS = os.cpu_count()
+
+    def run(self, meta: Meta, task_node: TaskNode[T]) -> T:
+        pass  # TODO(Assignment 9)
