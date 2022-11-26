e3892a3a548bf97e70cea080645738b6c1880712
Daniilmipt <sadovnikov.diu@phystech.edu>
2022-11-26 22:11:22 +0300

    hw 2 add commits

--- stem_framework/stem/core.py
+++ stem_framework/stem/core.py
@@ -3,6 +3,9 @@ import re
 
 
 def pascal_case_to_snake_case(name: str) -> str:
+    """
+    Return current class in snake_case
+    """
     name = re.sub("(.)([A-Z][a-z]+)", r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', name).lower()
 
@@ -12,6 +15,9 @@ class Named:
 
     @property
     def name(self):
+        """
+        Returns value of private variable _name if it is not None and returns name of current class in snake_case
+        """
         if self._name is not None:
             return self._name
         else:
@@ -19,4 +25,7 @@ class Named:
 
 
 class Dataclass(Protocol):
+    """
+    Dataclass protocol
+    """
     __dataclass_fields__: Any
--- stem_framework/stem/meta.py
+++ stem_framework/stem/meta.py
@@ -34,6 +34,9 @@ class MetaVerification:
     @staticmethod
     def verify(meta: Meta,
                specification: Optional[Specification] = None) -> "MetaVerification":
+        """
+        Check that meta have the same types as specification
+        """
 
         if is_dataclass(specification):
             spec_keys = specification.__dataclass_fields__.keys()
@@ -72,6 +75,9 @@ class MetaVerification:
 
 
 def get_meta_attr(meta: Meta, key: str, default: Optional[Any] = None) -> Optional[Any]:
+    """
+    Return meta value by key from top level of meta or default if key don't exist in meta
+    """
     try:
         return meta[key]
     except KeyError:
@@ -86,6 +92,9 @@ def get_meta_attr(meta: Meta, key: str, default: Optional[Any] = None) -> Option
 
 
 def update_meta(meta: Meta, **kwargs):
+    """
+    Update meta from kwargs
+    """
     if is_dataclass(meta):
         for key in kwargs:
             setattr(meta, key, kwargs[key])
