4b139efef74008197dfb493d16246f0fcc1a9d35
Daniilmipt <sadovnikov.diu@phystech.edu>
2022-12-10 16:00:00 +0300

    hw 10-11

--- stem_framework/tests/remote/test_distrubutor.py
+++ stem_framework/tests/remote/test_distrubutor.py
@@ -1,11 +1,12 @@
 import logging
+import threading
 import time
 import socket
 from unittest import TestCase
 
 from stem.envelope import Envelope
 from stem.remote.distributor import start_distributor_in_subprocess
-from stem.remote.unit import start_unit_in_subprocess, Commands
+from stem.remote.unit import start_unit_in_subprocess
 from tests.example_workspace import IntWorkspace
 
 HOST = "localhost"
@@ -21,16 +22,18 @@ class TestDistributor(TestCase):
         self.powerfullity = 0
         for i in range(1, 4):
             port = PORT+i
+            print(HOST, port)
             unit = start_unit_in_subprocess(IntWorkspace, HOST, port, i)
             self.units.append(unit)
             servers.append((HOST, port))
             self.powerfullity += i
 
         self.process = start_distributor_in_subprocess(HOST, PORT, servers=servers)
-        time.sleep(3) # Wait start servers
+        time.sleep(3)
 
     def _send(self, envelope: Envelope):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            print(HOST, PORT)
             sock.connect((HOST, PORT))
             sock.sendall(envelope.to_bytes())
             response = sock.recv(1024)
@@ -38,16 +41,26 @@ class TestDistributor(TestCase):
 
     def test_powerfullity(self):
         for i in range(5):
-            response = self._send(Commands.powerfullity)
+            print(i)
+            response = self._send(Envelope(dict(command="run", task_path='int_scale'), b'\xff\xff\xff\xffXabccc'))
             envelope = Envelope.from_bytes(response)
+            print(envelope.meta, envelope.data)
             meta = envelope.meta
-            if meta["status"] == "success":
-                self.assertEqual(meta["powerfullity"], self.powerfullity)
-            elif meta["status"] == "failed":
-                print(meta["error"])
+            # if meta["status"] == "success":
+            #     self.assertEqual(meta["powerfullity"], self.powerfullity)
+            # elif meta["status"] == "failed":
+            #     print(meta["error"])
 
-    def tearDown(self) -> None:
-        self._send(Envelope(dict(command="stop")))
-        for unit in self.units:
-            unit.terminate()
-        self.process.join()
+    # def tearDown(self) -> None:
+    #     print(threading.active_count())
+    #     print(threading.current_thread())
+    #     self._send(Envelope(dict(command="stop")))
+    #     print(threading.active_count())
+    #
+    #     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    #         sock.connect((HOST, PORT))
+    #         print('a')
+    #     # for unit in self.units:
+    #     #     unit.terminate()
+    #     # print(threading.active_count())
+    #     # self.process.join()
--- stem_framework/tests/remote/test_unit.py
+++ stem_framework/tests/remote/test_unit.py
@@ -16,31 +16,44 @@ logging.root.setLevel(logging.DEBUG)
 
 class ServerUnitTest(TestCase):
     def test_start_unit(self):
+        PORT = 9800
         start_unit(IntWorkspace, HOST, PORT, POWERFULLITY)
-
 
 
 class UnitHandlerTest(TestCase):
 
     def setUp(self) -> None:
         self.process = start_unit_in_subprocess(IntWorkspace, HOST, PORT, POWERFULLITY)
-        self.process.run()
-        time.sleep(3.0) # Wait start server
-        print(1)
+        time.sleep(3.0)  # Wait start server
 
     def _send(self, envelope: Envelope):
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
             sock.connect((HOST, PORT))
+            # print(envelope.to_bytes())
             sock.sendall(envelope.to_bytes())
+            print(envelope.to_bytes())
             response = sock.recv(1024)
             return response
 
     def test_powerfullity(self):
-        for i in range(5):
-            response = self._send(Envelope(dict(command="powerfullity")))
+        for i in range(3):
+            print('---------------')
+            print(i)
+            response = self._send(Envelope(
+                dict(command="run", task_path='int_scale'), b'\xff\xff\xff\xffXabccc'))
             envelope = Envelope.from_bytes(response)
-            self.assertEqual(envelope.meta["powerfullity"], POWERFULLITY)
+            print(envelope.meta)
 
-    def tearDown(self) -> None:
-        self._send(Envelope(dict(command="stop")))
-        self.process.join()
+            response = self._send(Envelope(
+                dict(command="stop")))
+            envelope = Envelope.from_bytes(response)
+            print(envelope.meta)
+
+            response = self._send(Envelope(
+                dict(command="structure")))
+            envelope = Envelope.from_bytes(response)
+            print(envelope.meta)
+
+    # def tearDown(self) -> None:
+    #     self._send(Envelope(dict(command="stop")))
+    #     self.process.join()
